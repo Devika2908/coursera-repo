827d5f2082bfa61e8e90fcdffaa36b9e091422fb
Devika2908 <devika.agrawal@cognizant.com>
2025-10-07 10:23:43 +0530

    Devika - First User Story

--- pages/EnterpriseResourcesPage.ts
+++ pages/EnterpriseResourcesPage.ts
@@ -2,6 +2,35 @@ import { Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 import allLocators from '../locators/locators.json';
 
-export class EnterpriseResourcesPage extends BasePage{
-    
+export class EnterpriseResourcesPage extends BasePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+  }
+
+  async clickResources() {
+    const res = this.page.locator(allLocators.EnterpriseResourcesPage.resources);
+    await res.waitFor({ state: 'visible' });
+    await res.click();
+  }
+
+  async clickArticles() {
+    const art = this.page.locator(allLocators.EnterpriseResourcesPage.articles).first();;
+    await art.waitFor({ state: 'visible' });
+    await art.click();
+  }
+
+ async selectTopic(topic: string) {
+    const dropdown = this.page.locator(allLocators.EnterpriseResourcesPage.topicDropdown);
+    await dropdown.click();
+
+    // pick the first matching element inside the dropdown
+    const option = this.page.locator(`text=${topic}`);
+    await option.click();
+}
+
+
+ 
 }
--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,7 +1,27 @@
-import { Page } from '@playwright/test';
+   import { Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 import allLocators from '../locators/locators.json';
 
-export class HomePage extends BasePage{
-    
+export class HomePage extends BasePage {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        super(page);
+        this.page = page;
+    }
+
+    async clickForUniversities() {
+        // Use locator from locators.json (can be text or xpath)
+        const uniLink = this.page.locator(allLocators.HomePage.forUniversities);
+        await uniLink.waitFor({ state: 'visible', timeout: 60000 }); // wait up to 60s
+        await uniLink.click();
+    }
+
+    async navigateToForEnterprise() {
+    const forEnterprise = this.page.locator(allLocators.HomePage.forEnterprises);
+    await forEnterprise.scrollIntoViewIfNeeded();
+    await forEnterprise.click();
+    // wait for something specific on the new page
+    await this.page.locator(allLocators.EnterpriseResourcesPage.resources).waitFor({ state: 'visible', timeout: 30000 });
+}
 }
