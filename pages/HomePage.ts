397cb4360fd3f17a73b7ee1f7d166d1c5dbec010
Kumari <2438022@cognizant.com>
2025-10-07 10:09:23 +0530

    Anushka - First User Story

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -3,5 +3,31 @@ import { BasePage } from './BasePage';
 import allLocators from '../locators/locators.json';
 
 export class HomePage extends BasePage{
-    
+
+
+
+  readonly page: Page;
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+  }
+
+  async selectGrowRole() {
+ const btn = this.page.locator(allLocators.HomePage.growRoleOption);
+    await btn.scrollIntoViewIfNeeded();
+    await btn.click({ force: true });
+
+  }
+
+  
+ async searchCourse(courseName: string) {
+
+        const searchBox = this.page.locator(allLocators.HomePage.whatDoYouWantToLearn);
+
+        await searchBox.fill(courseName);
+
+        await searchBox.press('Enter');
+
+    }
 }
