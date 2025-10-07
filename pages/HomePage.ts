98596fa1072305a5a89bd40177730dedfe18a0e0
Nandha Kishore <2438029@cognizant.com>
2025-10-07 10:01:31 +0530

    Yoshni-First User Story

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -2,6 +2,22 @@ import { Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 import allLocators from '../locators/locators.json';
 
-export class HomePage extends BasePage{
+export class HomePage extends BasePage {
     
+    //Web Dev Course
+    async searchForTerm(term: string) {
+        await this.page.fill(allLocators.HomePage.searchBar, term);
+        await this.page.click(allLocators.HomePage.webDev);
+    }
+
+    //Front End Course
+    async clickExplore() {
+        await this.page.locator(allLocators.HomePage.exploreButton).click();
+    }
+
+    async clickExploreRolesViewAll() {
+        await this.page.locator(allLocators.HomePage.exploreRolesViewAll).click();
+    }
+
 }
+
