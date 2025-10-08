abb37eb11fbdeb54c452af68b03c4b8ef9c17cfb
Kumari <2438022@cognizant.com>
2025-10-08 17:58:24 +0530

    Anushka - Final Commit

--- pages/OnlineDegreePage.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from '@playwright/test';
-import { BasePage } from './BasePage';
-import allLocators from '../locators/locators.json';
-
-export class OnlineDegreePage extends BasePage{
-    
-}
